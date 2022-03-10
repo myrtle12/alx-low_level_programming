9a30dde5b610959fbaef3c3b62fe8b6d9f3513a0
Esther <estheroginni@gmail.com>
2022-03-10 12:55:35 -0800

    a C program that prints exactly 'with proper grammer, but the outcome is a piece of art'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammer,
+ * but the outcome is a piece of art,
+ * Return zero if well executed, and non-zero if not
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is apiece of art,\n");
+	return (0);
+}
