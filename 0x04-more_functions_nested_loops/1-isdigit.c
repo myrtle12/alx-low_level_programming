58df5da2ed128282c7d9a1f7b8adfc0320391a0b
myrtle12 <88433636+myrtle12@users.noreply.github.com>
2022-03-18 04:54:18 +0100

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * _isdigit - check the code for Holberton School students.
+ * _isdigit - check the code for digits.
  * @c: Integer input
  * Return: Always 0.
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return 1;
-	else
-		return 0;
+	return (c >= '0' && c <= '9');
 }
