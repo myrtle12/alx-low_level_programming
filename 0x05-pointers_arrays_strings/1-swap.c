1ebfd0c14aa0880ec69615586f0ae89e68696ad9
Esther <estheroginni@gmail.com>
2022-03-21 18:46:40 -0700

    using pointers to swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int swaps the values of two integers
+ * swap_int - swaps the values of two integers
  * @a: pointer to the first value
  * @b: pointer to the second value
  */
