c87101453de144da8aff1b9deb0fc85726dd137c
Esther <estheroginni@gmail.com>
2022-03-21 18:24:57 -0700

    resetting pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - a function that takes a pointer to an int as parameter
+ * and updates the value it prints to 98
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Use of Pointers and Arrays in C
