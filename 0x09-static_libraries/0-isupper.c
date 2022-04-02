15c87c18ce193c94a69663fa53af46e8fd7afbba
Esther <estheroginni@gmail.com>
2022-04-02 01:59:02 -0700

    Static Libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - check the code for upper case characters
+ * @c: Character input
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code for digits.
+ * @c: Integer input
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
