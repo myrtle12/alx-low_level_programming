098072beb2559f0aa2a8ac93e81540b2b89e36f0
Esther <estheroginni@gmail.com>
2022-04-28 16:57:39 -0700

    program gives the number of bits that would be flipped from one num to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - number of bits you would need to flip
- * to get from one number to another
+ * flip_bits - number of bits to be flipped
+ *to get from one number to another
  * @n: parameter
  * @m: destiny
  * Return: nflips
